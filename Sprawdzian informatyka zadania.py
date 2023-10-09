f2275ba41b52dc91e93a4e49259970eca511fd3a
Knutoslav12 <93091125+Knutoslav12@users.noreply.github.com>
2023-10-09 22:25:33 +0200

    Add files via upload

---
+++ Sprawdzian informatyka zadania.py
@@ -0,0 +1,20 @@
+# zadanie 1
+tekst = input("Tekst: ")
+ilosc_znakow = len(tekst)
+print("Ilość znaków:", ilosc_znakow)
+for i in range(1, ilosc_znakow, 2):
+    print(tekst[i])
+
+# zadanie 2
+# dwa pierwsze podpunkty są te same
+print(tekst[::-1])
+
+# zadanie 3 (prawdopodobnie będzie inne)
+# znienne tekst i ilosc_znakow bierzemy z zadania 1
+tekst_powiekszony = '' 
+for i in range(0, ilosc_znakow, 1):
+    if i % 2 == 0:
+        tekst_powiekszony += tekst[i].upper()
+    else:
+        tekst_powiekszony += tekst[i]
+print(tekst_powiekszony)
