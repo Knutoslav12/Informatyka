b0491b90ade6c2b987a708b5f11ee3a0072e67dd
Knutoslav12 <93091125+Knutoslav12@users.noreply.github.com>
2023-11-28 13:02:43 +0100

    Update sortowanie przez wstawienie.py

--- sortowanie przez wstawienie.py
+++ sortowanie przez wstawienie.py
@@ -20,3 +20,13 @@ for i in range(1, n):
         j = j - 1
     liczby[j+1] = pom
 print(f'Posortowane liczby to: {liczby}')
+
+# sortowanie malejące
+for i in range(1, n):
+    pom = liczby[i]     # zmienna pomocnicza (element z listy)
+    j = i-1
+    while j >= 0 and liczby[j] < pom:
+        liczby[j+1] = liczby[j]
+        j = j - 1
+    liczby[j+1] = pom
+print(f'Posortowane malejąco liczby to: {liczby}')
